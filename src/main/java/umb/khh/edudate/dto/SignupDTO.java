351081eca700b9c93f58872aa0dd07bcc1bf072b
ihryshchenko <ihryshchenko@student.umb.sk>
2024-05-26 22:15:31 +0200

    update

--- src/main/java/umb/khh/edudate/dto/SignupDTO.java
+++ src/main/java/umb/khh/edudate/dto/SignupDTO.java
@@ -2,7 +2,42 @@ package umb.khh.edudate.dto;
 
 import lombok.Getter;
 
-@Getter
+
 public record SignupDTO(String username, String name, String surname, String dateOfBirth, String email, String password, String faculty, String profileDescription, String[] interest) {
 
+    public String getUsername() {
+        return username;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public String getDateOfBirth() {
+        return dateOfBirth;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public String getFaculty() {
+        return faculty;
+    }
+
+    public String getProfileDescription() {
+        return profileDescription;
+    }
+
+    public String[] getInterest() {
+        return interest;
+    }
 }
